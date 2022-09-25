350b18a82a0a29183767375e2fa5a525a956a4cd
Narudon Sookkheha <narusuke15@gmail.com>
2022-09-25 11:35:59 +0700

    update material path

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class FlashlightController : MonoBehaviour
 {
+    [Header("Scene References")]
     public GameObject FlashlightGO;
 
+    [Header("Parameters")]
     public bool IsOn = true;
+
 
     void Start()
     {
