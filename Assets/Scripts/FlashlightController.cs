d804ba472ec79b07115a0059ff67d2107e3f5e39
Narudon Sookkheha <narusuke15@gmail.com>
2022-09-25 11:59:54 +0700

    update flashlight comment

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
 
+/// <Summary>
+/// Class ที่เอาไว้ใช้ควบคุมไฟฉาย
+/// </Summary>
 public class FlashlightController : MonoBehaviour
 {
     [Header("Scene References")]
-    public GameObject FlashlightGO;
+    public GameObject FlashlightGO; // ประกาศตัวแปรเพื่อ Reference ค่า GameObject ไฟฉายใน Scene
 
     [Header("Parameters")]
-    public bool IsOn = true;
+    public bool IsOn = true; // ตัวแปรเช็คว่าไฟฉายเปิดอยู่หรือไม่
 
 
     void Start()
     {
-        FlashlightGO.SetActive(IsOn);
+        FlashlightGO.SetActive(IsOn); // เปิด-ปิดไฟฉายตามค่า IsOn ด้วยการเปิด-ปิด GameObject ตอน scripts ทำงานครั้งแรก
     }
 
     void Update()
     {
+        // เช็คทุกเฟรมว่าผู้เล่นกด 'F' หรือไม่
+        // --- ถ้า condition ในวงเล็บเป็น 'true' จะทำงานในปีกา {} 
+        // --- Input.GetKeyDown() จะส่งค่า bool มาให้เราว่าเรากดปุ่มนี้หรือไม่ ทำให้เราสามารถใช้เช็ค condition ใน if ได้เลย
         if (Input.GetKeyDown(KeyCode.F))
         {
-            IsOn = !IsOn;
-            FlashlightGO.SetActive(IsOn);
+            IsOn = !IsOn; // เปลี่ยนค่า boolean เป็นค่าตรงข้ามของตัวเอง 
+            // --- (สัญลักษณ์ '!' จะเปลี่ยนค่า จริง-เท็จ ให้เป็นตรงข้าม )
+            FlashlightGO.SetActive(IsOn); // เปิด-ปิดไฟฉายตามค่า IsOn ด้วยการเปิด-ปิด GameObject
+            // --- SetActive(bool) เป็น Function ของ Component GameObject ที่ใช้เปิด-ปิด Object ได้
         }
     }
 }
