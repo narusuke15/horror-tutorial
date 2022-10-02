c6c597636adf9c8194ae19597c76e8878c095a2e
Narudon Sookkheha <narusuke15@gmail.com>
2022-10-02 21:18:40 +0700

    update scripts comment

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,63 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using TMPro; // การจะเรียกใช้ TextMeshPro ต้องประกาศการเรียกใช้ namespace นี้
 
+/// <summary>
+/// Scripts ควบคุม interaction และ animation ของประตู
+/// </summary>
 public class Door : MonoBehaviour
 {
-    public TMP_Text ActionText;
-    public GameObject Crosshair;
-    Animator _doorAnimator;
-    Player _playerComponent;
-    bool _isOpen;
+    [Header("References")]
+    public TMP_Text ActionText; // text ที่จะขึ้นถ้าเราสามารถเปิดปิดประตูได้
+    public GameObject Crosshair; // เป้าที่จะขึ้นตอนเปิดปิดประตูได้
+    Animator _doorAnimator; // Animator ที่ควบคุม animation การเปิดปิดของประตู
+    Player _playerComponent; // ดึงมาเรียกใช้ข้อมูล raycast ของ Player
+
+    bool _isOpen; // เก็บค่าการเปิดปิดของประตูเพื่อเอาไปควบคุม Animator ของประตู
 
     void Start()
     {
+        // ใช้โค้ด referencs component
+        // FindObjectOfType<T>() >> ทำการหา component ใน Scene ที่มี type ใน <...>
+        // -- เหมาะกับ scene ที่มี component อันนี้อันเดียว
         _playerComponent = FindObjectOfType<Player>();
+        // GetComponent<T>() ใช้หา component ที่อยู่ใน GameObject ที่ script นี้ติดอยู่
+        // -- เหมาะกับการเรียก component ที่เรารู้ว่ามันอยู่ใน GameObject เดียวกัน
         _doorAnimator = GetComponent<Animator>();
     }
 
-    // function พิเศษ ทำงานเมื่อชี้เมาส์ไปที่วัตถุที่ component นี้เกาะอยู่
+    // function พิเศษ ทำงานตลอดเวลาเมื่อชี้เมาส์ไปที่วัตถุที่ component นี้เกาะอยู่
     void OnMouseOver()
     {
+        // ถ้าระยะของผู้เล่นกับวัตถุน้อยกว่า 1.8 เมตร
         if (_playerComponent.DistanceFromTarget < 1.8f)
         {
+            // เปิด action UI
             ActionText.gameObject.SetActive(true);
             Crosshair.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                _isOpen = !_isOpen;
-                _doorAnimator.SetBool("IsOpen", _isOpen);
-                StartCoroutine(DisableColliderRoutine(0.5f));
-            }
 
+            // แสดง ActionText เปิดปิดประตู
             if (_isOpen)
                 ActionText.text = "- Close Door -";
             else
                 ActionText.text = "- Open Door -";
+
+            // เปิดปิดประตูเมื่อผู้เล่นกด 'e'
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _isOpen = !_isOpen; // สลับค่าเปิดปิด
+                _doorAnimator.SetBool("IsOpen", _isOpen); // ปรับ parameter 'IsOpen' ใน Animator (เดี๋ยวมันจัดการที่เหลือให้เอง)
+                StartCoroutine(DisableColliderRoutine());
+            }
         }
+        // ซ่อน Action UI ถ้าระยะห่างเกิน 8 เมตร
         else
         {
             HideActionUI();
         }
     }
 
+    // function พิเศษ ทำงานตลอดเวลาเมื่อชี้เมาส์ออกจากวัตถุที่ component นี้เกาะอยู่
     void OnMouseExit()
     {
-        HideActionUI();
+        HideActionUI(); // ซ่อน Action UI
     }
 
+    // ปิด action UI
     void HideActionUI()
     {
         ActionText.gameObject.SetActive(false);
         Crosshair.SetActive(false);
     }
 
-    IEnumerator DisableColliderRoutine(float delaySeconds)
+    // พยายามแก้บัคโดนประตูดีดตก map ด้วยการปิด collider ชั่วคราว
+    IEnumerator DisableColliderRoutine()
     {
         Collider colliderComponent = GetComponent<Collider>();
         colliderComponent.enabled = false;
-        yield return new WaitForSeconds(delaySeconds);
+        yield return new WaitForSeconds(0.5f); // รอ 0.5 วิ (= เวลาตอนประตูเคลื่อนที่)
         colliderComponent.enabled = true;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,34 @@
 using UnityEngine;
 
+/// <summary>
+/// Class ควบคุม Player
+/// </summary>
 public class Player : MonoBehaviour
 {
     [Header("References")]
-    public Transform PlayerTransform;
+    public Transform PlayerTransform; // อ้างอิงข้อมูลตำแหน่งจาก component transform
 
-    public static GameObject HitRef;
-    
-    [Header("Debug")]
-    public float DistanceFromTarget;
-    public string HitName;
+    [Header("Debug (เอาไว้แสดงผลอย่างเดียว)")]
+    public float DistanceFromTarget; // แสดงระยะห่างของผู้เล่นกับวัตถุที่ชน
+    public string HitName; // แสดงชื่อของวัตถุทีชน
 
     void Update()
     {
-        RaycastHit hit;
+        RaycastHit hit; // ประกาศตัวแปร RaycastHit เพื่อเอาไว้ส่งไปรับค่าการเช็คชนจาก Physics.Raycast(...) ได้
+        // bool Physics.Raycast(...) เอาไว้ยิงเส้นไปเช็คว่าเส้นนี้ไปชนกับ GameObject ที่มี Collider หรือไม่ และส่งค่า true, false กลับมา
+        // --- [p1] PlayerTransform.position >> แทนจุดเริ่มต้นของเส้นที่จะยิง
+        // --- [p2] PlayerTransform.TransformDirection(Vector3.forward)​ >> แทนทิศทางของเส้นที่จะยิง
+        // --- [p3] out hit >> ถ้ามีการชนจะส่งข้อมูลของวัตถุที่ชนกลับมาให้ตัวแปร hit สำหรับเอามาใช้งานต่อได้
         if (Physics.Raycast(PlayerTransform.position, PlayerTransform.TransformDirection(Vector3.forward), out hit))
         {
-            DistanceFromTarget = hit.distance;
-            HitName = hit.collider.name;
-            HitRef = hit.collider.gameObject;
+            DistanceFromTarget = hit.distance; // แสดงระยะห่างของผู้เล่นกับวัตถุที่ชน
+            HitName = hit.collider.name; // แสดงชื่อของวัตถุทีชน
         }
+        // ถ้า Raycast ไม่โดนอะไรเลยให้แสดงค่าตามด้านล่าง (เอาไว้ช่วย Debug)
         else
         {
             DistanceFromTarget = 0;
             HitName = "none";
-            HitRef = null;
         }
     }
 }
