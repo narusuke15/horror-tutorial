51ca9cb413f03536304738fbca25bcc3f7829936
Narudon Sookkheha <narusuke15@gmail.com>
2022-10-09 16:41:01 +0700

    add switch flashlight from FlashLightPRO

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -6,17 +6,8 @@ using UnityEngine;
 // [Tips] โดยทั่วไปเราจะใช้ชื่อ class ที่ขึ้นต้นและคั่นคำต่อไปด้วยตัวพิมพ์ใหญ่ เราเรียกวิธีการพิมพ์แบบนี้ว่า Pascal Casing
 public class FlashlightController : MonoBehaviour
 {
-    [Header("Scene References")]
-    public GameObject FlashlightGO; // ประกาศตัวแปรเพื่อ Reference ค่า GameObject ไฟฉายใน Scene
-
-    [Header("Parameters")]
-    public bool IsOn = true; // ตัวแปรเช็คว่าไฟฉายเปิดอยู่หรือไม่
-
-
-    void Start()
-    {
-        FlashlightGO.SetActive(IsOn); // เปิด-ปิดไฟฉายตามค่า IsOn ด้วยการเปิด-ปิด GameObject ตอน scripts ทำงานครั้งแรก
-    }
+    [Header("References")]
+    public FlashlightPRO Flashlight; // ประกาศตัวแปรเพื่อ Reference ค่า FlashlightPRO ไฟฉายใน Scene
 
     void Update()
     {
@@ -25,10 +16,7 @@ public class FlashlightController : MonoBehaviour
         // --- Input.GetKeyDown() จะส่งค่า bool มาให้เราว่าเรากดปุ่มนี้หรือไม่ ทำให้เราสามารถใช้เช็ค condition ใน if ได้เลย
         if (Input.GetKeyDown(KeyCode.F))
         {
-            IsOn = !IsOn; // เปลี่ยนค่า boolean เป็นค่าตรงข้ามของตัวเอง 
-            // --- (สัญลักษณ์ '!' จะเปลี่ยนค่า จริง-เท็จ ให้เป็นตรงข้าม )
-            FlashlightGO.SetActive(IsOn); // เปิด-ปิดไฟฉายตามค่า IsOn ด้วยการเปิด-ปิด GameObject
-            // --- SetActive(bool) เป็น Function ของ Component GameObject ที่ใช้เปิด-ปิด Object ได้
+            Flashlight.Switch(); // เปิด-ปิดไฟฉาย
         }
     }
 }
