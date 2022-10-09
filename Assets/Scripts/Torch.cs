5df2fb053255c1e4d21955d069d6426c6595ac26
Narudon Sookkheha <narusuke15@gmail.com>
2022-10-09 11:48:47 +0700

    update torch scripts

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -7,8 +7,10 @@ public class Torch : MonoBehaviour
     [Header("Parameters")]
     public float MinIntesity = 0f;
     public float MaxIntesity = 2f;
-    public float Speed = 0.1f;
+    public float MinSpeed = 0.1f;
+    public float MaxSpeed = 0.2f;
 
+    float _currentSpeed;
     Light _lightComponent;
     bool _isUp = false;
 
@@ -16,6 +18,7 @@ public class Torch : MonoBehaviour
     {
         _lightComponent = transform.GetComponentInChildren<Light>();
         _lightComponent.intensity = Random.Range(MinIntesity, MaxIntesity); // สุ่มความสว่างของแสง
+        RandomIntensitySpeed();
     }
 
     void Update()
@@ -23,16 +26,27 @@ public class Torch : MonoBehaviour
         if (_isUp)
         {
             if (_lightComponent.intensity < MaxIntesity)
-                _lightComponent.intensity += Speed;
+                _lightComponent.intensity += _currentSpeed;
             else
+            {
                 _isUp = false;
+                RandomIntensitySpeed();
+            }
         }
         else
         {
             if (_lightComponent.intensity > MinIntesity)
-                _lightComponent.intensity -= Speed;
+                _lightComponent.intensity -= _currentSpeed;
             else
+            {
                 _isUp = true;
+                RandomIntensitySpeed();
+            }
         }
+    }
+
+    void RandomIntensitySpeed()
+    {
+        _currentSpeed = Random.Range(MinSpeed, MaxSpeed);
     }
 }
